e65c166e1d922d8d96a650179382ee33d50f4eef
wlillisdev <williamlillis100@gmail.com>
2022-10-12 09:18:15 +0000

    added views to adjust in bad and subtotal in bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render, redirect
+'''
+Imports relevant django packages
+'''
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+    )
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -23,3 +30,41 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    '''
+    Adjust the quantity of items in the shopping bag
+    '''
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}'
+        )
+    else:
+        bag.pop(item_id)
+        messages.success(
+            request, f'{product.name} removed from your shopping bag'
+        )
+
+    request.session['bag'] = bag
+
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    '''
+    Adjust the quantity of items in the shopping bag
+    '''
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+    bag.pop(item_id)
+    messages.success(request, f'{product.name} removed from your shopping bag')
+
+    request.session['bag'] = bag
+
+    return HttpResponse(status=200)
