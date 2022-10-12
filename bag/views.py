b4b403ee36f8ef6eb5dfc9b046f10eef2c36a6b9
wlillisdev <williamlillis100@gmail.com>
2022-10-12 12:46:45 +0000

    added  toasts and set up div html to display alerts

--- bag/views.py
+++ bag/views.py
@@ -19,9 +19,11 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    messages.success(request, f'Added {product.name} to your bag')
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
