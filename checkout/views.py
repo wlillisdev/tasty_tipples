b6d4b6f9ffb037d79cc0abb8931bd4586c1342a7
wlillisdev <williamlillis100@gmail.com>
2022-10-15 09:14:15 +0000

    added inital stripe elements and js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Lt5hOIzHeAiCIve2jawM4Mnp5paaLXRmTghoO68yjQYzUDOzJqASOKdIJrUwUAx2noAbkd5gU635hgDS4LWvsJV00D0JaI7bV',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
