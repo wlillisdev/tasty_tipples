774e1133ac2f01409b8b6a270d64d28e6802d924
wlillisdev <williamlillis100@gmail.com>
2022-10-15 15:18:17 +0000

    finished setting up webhook haandlers and tested

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,11 @@
 import uuid
-
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
 from products.models import Product
+
+
+
 
 
 class Order(models.Model):
@@ -22,6 +23,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
