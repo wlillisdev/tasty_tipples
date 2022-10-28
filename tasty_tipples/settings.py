6c9027d5712df7f48163a9517b34718be119ce5e
wlillisdev <williamlillis100@gmail.com>
2022-10-28 14:09:24 +0000

    replaced the secret key

--- tasty_tipples/settings.py
+++ tasty_tipples/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0m37+wif%bpy_ly+sk5(^ry251=$nw0&0oe)y!$4$ja&%0k1bp'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
