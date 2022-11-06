8a4a17bfebe8595b4303bb65d7837cc2a9006f90
wlillisdev <williamlillis100@gmail.com>
2022-11-06 17:36:24 +0000

    delete unused imports after flake 8 run

--- contactus/views.py
+++ contactus/views.py
@@ -13,7 +13,9 @@ def contact(request):
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.success(request, 'Thank you. Your email has been sent. We will get back to you as soon as possible.')
+            messages.success(request, 'Thank you. Your email has been sent. \
+                                       We will get back to you \
+                                       as soon as possible.')
         else:
             messages.error(request, 'Sorry, your email could not be sent.')
     form = ContactForm()
