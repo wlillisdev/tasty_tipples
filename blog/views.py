db83be30cd298c7812455a784a2887946abe826e
wlillisdev <williamlillis100@gmail.com>
2022-11-06 18:21:20 +0000

    adding color to hamburger menu

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 from django.views.generic import ListView
 
 
@@ -9,17 +10,6 @@ class BlogView(ListView):
     model = Post
     template_name = 'blog/blog_list.html'
     ordering = ['-created_on']
-
-
-# def blog_list(request):
-#     """blog list"""
-#     posts = Post.objects.all().order_by('-created_on')
-
-#     context = {
-#         'posts': posts,
-#     }
-
-#     return render(request, 'blog/blog_list.html', context)
 
 
 def blog_detail(request, slug):
